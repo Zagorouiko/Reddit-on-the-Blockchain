9a149b9d582c8a44ddc778a6f8d6d328920b207f
Zagorouiko <Zagorouiko@gmail.com>
2022-10-03 09:16:26 -0700

    updated package json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "676ffb24eb31e640"; }
+/******/ 		__webpack_require__.h = function() { return "0ae590d92673b26b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
